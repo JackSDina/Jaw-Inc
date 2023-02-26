4c7b79f70197a8d654471964fd0a6e6ff563488e
JackSDina <112967159+JackSDina@users.noreply.github.com>
2023-02-25 19:37:48 -0500

    Add files via upload

---
+++ GameCheckDriver.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class GameCheckDriver {
+
+	public static void main(String[] args) {
+		Map<Game,Integer> mainMap = new HashMap();
+	}
+	
+	void searchName (String name) {
+		//TODO
+	}
+	
+	void searchPrice (float price) {
+		//TODO
+	}
+	
+	void searchPublisher (String publisher) {
+		//TODO
+	}
+	
+	void searchGenre (Set<Integer> genre) {
+		//TODO
+	}
+	
+	void searchPlatforms (Set<Integer> platforms) {
+		//TODO
+	}
+	
+	void searchRating (int ratings) {
+		//TODO
+	}
+}
