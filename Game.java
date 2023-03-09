3b6980bf6cc6e86b927eaf9c35c443d934701f0a
jackmcmaken <112977678+jackmcmaken@users.noreply.github.com>
2023-03-09 16:33:39 -0500

    Added equals() method

--- Game.java
+++ Game.java
@@ -7,78 +7,87 @@ import java.util.*;
 
 public class Game {
 
-	String name;
-	float price;
-	String publisher;
-	Set<Integer> genres;
-	Set<Integer> platforms;
-	int rating;
+    String name;
+    float price;
+    String publisher;
+    Set<Integer> genres;
+    Set<Integer> platforms;
+    int rating;
 
-	/**
-	 * Constructor for the game object.
-	 * @param n Name string.
-	 * @param pr Price float.
-	 * @param pb Publisher string.
-	 * @param g Set of integers representing game genres.
-	 * @param pl Set of integers representing platforms.
-	 * @param r Rating integer.
-	 */
-	public Game(String n, float pr, String pb, Set<Integer> g, Set<Integer> pl,
-	int r) {
-		name = n;
-		price = pr;
-		genres = g;
-		platforms = pl;
-		rating = r;
-	}
+    /**
+     * Constructor for the game object.
+     * @param n Name string.
+     * @param pr Price float.
+     * @param pb Publisher string.
+     * @param g Set of integers representing game genres.
+     * @param pl Set of integers representing platforms.
+     * @param r Rating integer.
+     */
+    public Game(String n, float pr, String pb, Set<Integer> g, Set<Integer> pl,
+    int r) {
+        name = n;
+        price = pr;
+        genres = g;
+        platforms = pl;
+        rating = r;
+    }
 
-	// Auto generated getters and setters
-	public String getName() {
-		return name;
-	}
+    // Auto generated getters and setters
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public float getPrice() {
-		return price;
-	}
+    public float getPrice() {
+        return price;
+    }
 
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    public void setPrice(float price) {
+        this.price = price;
+    }
 
-	public String getPublisher() {
-		return publisher;
-	}
+    public String getPublisher() {
+        return publisher;
+    }
 
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 
-	public Set<Integer> getGenres() {
-		return genres;
-	}
+    public Set<Integer> getGenres() {
+        return genres;
+    }
 
-	public void setGenres(Set<Integer> genres) {
-		this.genres = genres;
-	}
+    public void setGenres(Set<Integer> genres) {
+        this.genres = genres;
+    }
 
-	public Set<Integer> getPlatforms() {
-		return platforms;
-	}
+    public Set<Integer> getPlatforms() {
+        return platforms;
+    }
 
-	public void setPlatforms(Set<Integer> platforms) {
-		this.platforms = platforms;
-	}
+    public void setPlatforms(Set<Integer> platforms) {
+        this.platforms = platforms;
+    }
 
-	public int getRating() {
-		return rating;
-	}
+    public int getRating() {
+        return rating;
+    }
 
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+    
+    public boolean equals(Object o) {
+        if (o == null || !(o instanceof Game)) {
+            return false;
+        }
+        
+        Game g = (Game)o;
+        return this.name.equals(g.name);
+    }
 
 }
