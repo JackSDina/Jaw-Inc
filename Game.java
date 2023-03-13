efae916c5d389bc966bd2be600ee505a234c6ef4
MrPres50 <55299519+MrPres50@users.noreply.github.com>
2023-03-13 17:59:33 -0400

    Added to string method

--- Game.java
+++ Game.java
@@ -32,6 +32,33 @@ public class Game {
         rating = r;
     }
 
+   // this method is used for the to string method
+   public static ArrayList<String> convertGenres(Set<Integer> valCode) {
+		// Empty list to be filed with valid genres
+		ArrayList<String> genreList = new ArrayList<String>();
+		// Reference list containing all genres in order according to their code
+		ArrayList<String> genres = new ArrayList<String>(Arrays.asList("Indie", "Profile Features Limited", "Action",
+				"Casual", "Singleplayer", "Adventure", "Simulation", "Strategy", "RPG", "2D", "Atmospheric", "3D",
+				"Puzzle", "Pixel Graphics", "Fantasy", "Story Rich", "Colorful", "Exploration", "Free to Play", "Cute",
+				"Multiplayer", "Early Access", "First-Person", "Arcade", "Anime", "Shooter"));
+		for (int i = 0; i < 26; i++) {
+			if (valCode.contains(i)) {
+				genreList.add(genres.get(i));
+			}
+		}
+		return genreList;
+	}
+	
+	public String toString() {
+        String fullObj = name + " Rating: " + rating + " Genre Tags:";
+        ArrayList<String> genreList = convertGenres(genres);
+        for (int i = 0; i < genreList.size() - 1; i++) {
+			fullObj += " " + genreList.get(i) + ",";
+		}
+        fullObj += " " + genreList.get(genreList.size() - 1);
+        return fullObj;
+    }
+    
     // Auto generated getters and setters
     public String getName() {
         return name;
